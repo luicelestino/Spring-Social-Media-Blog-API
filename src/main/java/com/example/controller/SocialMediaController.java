e89e7455146b45840f2df7d87424d23c1c9fbc95
Luigi Celestino <luigicelestino@yahoo.com>
2025-04-16 22:46:41 +0000

    Wrote methods in controller class

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,19 @@
 package com.example.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.azul.crs.client.Response;
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +21,59 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+ @RestController
 public class SocialMediaController {
+
+    private AccountService accountService;
+    private MessageService messageService;
+
+    // Create a new account
+    @PostMapping("register")
+    public ResponseEntity<Account> registerAccount(@RequestBody Account newAccount) {
+
+    }
+
+    // Login to an existing account
+    @PostMapping("login")
+    public ResponseEntity<Account> loginAccount(@RequestBody Account loginAccount) {
+
+    }
+
+    // Create a new message
+    @PostMapping("messages")
+    public ResponseEntity<Message> createMessage(@RequestBody Message newMessage) {
+
+    }
+
+    // Get all existing messages
+    @GetMapping("messages")
+    public ResponseEntity<Message> getMessage(@RequestBody Message message) {
+
+    }
+
+    // Retrieve a message by its ID
+    @GetMapping("messages/{messageId}")
+    public ResponseEntity<Message> getMessageById(@PathVariable Integer messageId) {
+
+    }
+
+    // Delete a message by its ID
+    @DeleteMapping("messages/{messageId}")
+    public ResponseEntity<Message> deleteMessageById(@PathVariable Integer messageId) {
+
+    }
+
+    // Update a message by its ID
+    @PatchMapping("messages/{messageId}")
+    public ResponseEntity<Message> updateMessageById(@PathVariable Integer messageId) {
+
+    }
+
+    // Get all messages written by a specific account by its ID
+    @GetMapping("accounts/{accountId}")
+    public ResponseEntity<Message> getAllMessagesByAccount(@PathVariable Integer accountId) {
+        
+    }
 
 }
